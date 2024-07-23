b6a1a72523d20f9bbca887d602b7b68bfa98038e
Ivan Corniel <ivancorniel30@gmail.com>
2024-07-23 17:46:16 -0400

    Update bash.sh

--- .github/bash.sh
+++ .github/bash.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-USER=$1
+set -e
 
-echo $USER
+COMENT=$1
+USER=$2
+APPROVERS='ivancorniel nishedcob Alan1108'
+APPROVAL='ok yes approved go'
+
+if ! echo $APPROVERS | tr ' ' '\n' | grep -q $USER || ! echo $APPROVAL | tr ' ' '\n' | grep -q $COMENT ; then
+  echo deployment not approved
+  exit 1
+else
+  echo $USER approved with $COMENT
+fi
