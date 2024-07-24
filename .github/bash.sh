8072e297dcace9c003c37f4b258dcd0c78709faf
Ivan Corniel <ivancorniel30@gmail.com>
2024-07-23 23:21:49 -0400

    Update bash.sh

--- .github/bash.sh
+++ .github/bash.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+TOKEN=$GITHUB_TOKEN
 COMENT=$1
 USER=$2
 APPROVAL='ok yes approved go'
-APPROVERS=$(curl -s -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" \
+APPROVERS=$(curl -s -H "Authorization: token $TOKEN" \
   "https://api.github.com/orgs/ivancorniel-org/members" | jq -r '.[].login')
 echo $APPROVERS
 # if ! echo $APPROVERS | tr ' ' '\n' | grep -q $USER || ! echo $APPROVAL | tr ' ' '\n' | grep -q $COMENT ; then
