2fe08647fbf568c3c64ec07b4ed98cb335e39d02
Ivan Corniel <ivancorniel30@gmail.com>
2024-07-26 09:38:32 -0400

    Update bash.sh

--- .github/bash.sh
+++ .github/bash.sh
@@ -6,14 +6,10 @@ TOKEN=$GITHUB_TOKEN
 COMENT=$1
 USER=$2
 APPROVAL='ok yes approved go'
-APPROVERS=$(curl -s -H "Authorization: token $TOKEN" \
-  "https://api.github.com/orgs/ivancorniel-org/members" | jq -r '.[].login')
+APPROVERS=ivancorniel
 
-return $APPROVERS
-  
 if ! echo $APPROVERS | tr ' ' '\n' | grep -q $USER || ! echo $APPROVAL | tr ' ' '\n' | grep -q $COMENT ; then
-  echo deployment not approved
-  exit 1
+  echo "DEPLOYMENT='NOT APPROVED'" >> "$GITHUB_ENV"
 else
-  echo $USER approved with $COMENT
+  echo "DEPLOYMENT='APPROVED'" >> "$GITHUB_ENV"
 fi
